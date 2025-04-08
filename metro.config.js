625c9d0cb3a8dee83e6a2366eb7063f1cf60f9aa
Vinod Sigadana <vinodsigadana@Vinods-MacBook-Pro.local>
2025-04-08 21:27:16 +0530

    Set node version and watchman, metro config

--- metro.config.js
+++ metro.config.js
@@ -6,6 +6,13 @@ const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
  *
  * @type {import('metro-config').MetroConfig}
  */
-const config = {};
+const config = {
+  watchFolders: [__dirname],
+  resolver: {
+    nodeModulesPaths: [__dirname + '/node_modules'],
+  },
+  maxWorkers: 2,
+  resetCache: true,
+};
 
 module.exports = mergeConfig(getDefaultConfig(__dirname), config);
